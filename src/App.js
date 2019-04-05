56907149a09ae715687db7c7a84f898e1f6f49a8
petersen <pmphareng@gmail.com>
2019-04-05 16:50:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      name: "",
+    }
+    this.setName = this.setName.bind(this);
+  }
+
+  setName(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <div className="form-inline">
+            <p>Enter Name: </p>
+            <input type="text" id="name" onChange={this.setName} />
+          </div>
+        </form>
+        <div style={{"margin":"20px"}}>
+          Your name is: {this.state.name}
+        </div>
       </div>
     );
   }
